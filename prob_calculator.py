961c9990c354a89f793d5adf42125749d4d2c2b0
April Milan <108895508+aprilmilanpdx@users.noreply.github.com>
2023-03-12 14:49:55 -0700

    mid-work break

--- prob_calculator.py
+++ prob_calculator.py
@@ -9,16 +9,33 @@ import random
 # hat3 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
 
 # A hat will always be created with at least one ball. 
+
 # The arguments passed into the hat object upon creation should be converted to a contents instance variable. 
+
 # contents should be a list of strings containing one item for each ball in the hat. Each item in the list should be a color name representing a single ball of that color.
+
 # For example, if your hat is {"red": 2, "blue": 1}, contents should be ["red", "red", "blue"].
 
 
 class Hat:
     def __init__(self, **balls):
-        self.contents = [key for key, value in balls.items() for i in range(value)]
+        self.contents = [k for k, v in balls.items() for i in range(v)]
 
-    
+    def draw(self, n):
+        drawn = []
+        if n > len(self.contents):
+            return self.contents
+        for i in range(n):
+            
+            
+
+# The Hat class should have a draw method that accepts an argument indicating the number of balls to draw from the hat. 
+
+# This method should remove balls at random from contents and return those balls as a list of strings. 
+
+# The balls should not go back into the hat during the draw, similar to an urn experiment without replacement. 
+
+# If the number of balls to draw exceeds the available quantity, return all the balls.
 
 
 # def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
